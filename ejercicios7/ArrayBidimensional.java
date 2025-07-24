c540c0d1fa582a89ff3d0007fb1e0c3b6387ad84
Mackenzi YT <202400898d@utea.edu.pe>
2025-07-24 11:49:25 -0500

    arrays Bidimentionals

---
+++ ejercicios7/ArrayBidimensional.java
@@ -0,0 +1,30 @@
+package ejercicios7;
+
+public class ArrayBidimensional {
+    public static void main(String[] args) {
+        //hacer un array bidimension
+        int [][] n = new int[3][2];
+
+        n[0][0] = 12;
+        n[0][1] = 15;
+        n[1][0] = 13;
+        n[1][1] = 11;
+        n[2][0] = 5;
+        n[2][1] = 8;
+        
+        int fila , columna;
+
+        for (fila  = 0; fila < 3; fila++) {
+
+            for (columna = 0; columna < 2; columna++) {
+
+                System.out.printf("%10d",n[fila][columna]);
+
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
